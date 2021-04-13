9e4455319823e31e57328fe3740e51dddb75cc14
Elexandro Torres <elexandro.torres@gmail.com>
2021-04-13 15:26:51 -0300

    Corrigido erro que finalizava ao clicar no botão

--- app/src/main/java/com/elexandro/imcapp/MainActivity.kt
+++ app/src/main/java/com/elexandro/imcapp/MainActivity.kt
@@ -5,42 +5,48 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
 
-    private var edt_peso: EditText? = null
-    private var edt_altura: EditText? = null
-    private var btn_calcular: Button? = null
-    private var txt_result: TextView? = null
+    private var edtPeso: EditText? = null
+    private var edtAlt: EditText? = null
+    private var btnCalc: Button? = null
+    private var txtResult: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edt_peso = findViewById(R.id.pesoEDT)
-        edt_altura = findViewById(R.id.alturaEDT)
-        txt_result = findViewById(R.id.resultTXT)
-        btn_calcular = findViewById(R.id.calcularBTN)
+        edtPeso = findViewById(R.id.pesoEDT)
+        edtAlt = findViewById(R.id.alturaEDT)
+        txtResult = findViewById(R.id.resultTXT)
+        btnCalc = findViewById(R.id.calcularBTN)
 
         setListeners()
     }
 
     private fun setListeners() {
 
-        btn_calcular?.setOnClickListener {
-            calcularIMC(edt_peso?.text.toString(), edt_altura?.text.toString())
+        //edtAlt?.doAfterTextChanged { text ->
+         //   Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        //}
+        btnCalc?.setOnClickListener {
+            //Toast.makeText(this, edtAlt?.text.toString(), Toast.LENGTH_SHORT).show()
+            calcularIMC(edtPeso?.text.toString(), edtAlt?.text.toString())
         }
-
 
     }
 
-    private fun calcularIMC(peso: String, altura: String) {
+    private fun calcularIMC(peso: String, alt: String) {
         val peso =  peso.toFloatOrNull()
-        val altura = altura.toFloatOrNull()
-        if(peso != null && altura != null) {
-            val imc = peso / (altura * altura)
-            txt_result?.text = "Seu imd: %.{2}f".format(imc)
+        val alt = alt.toFloatOrNull()
+        if(peso != null && alt != null) {
+            val imc = peso / (alt * alt)
+            txtResult?.text = "Seu imd: %.${2}f".format(imc)
+            //txtResult?.text = imc.toString()
+            Toast.makeText(this, imc.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
